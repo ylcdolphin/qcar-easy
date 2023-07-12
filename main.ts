12b749dad6be77e666dfbb88325e473899ecd426
ylcdolphin <33015665+ylcdolphin@users.noreply.github.com>
2023-07-12 22:13:00 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,29 @@
+let start = 0
+IR.IR_callbackUser(function (message) {
+    if (message == 1) {
+        start = 1
+    } else {
+        start = 0
+    }
+})
+function trail () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 75)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
+}
+basic.forever(function () {
+    if (start == 1) {
+        trail()
+        basic.pause(100)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
